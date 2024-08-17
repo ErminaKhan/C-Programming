55cdd36fd43e27f8fb4a822e4b4f3731fa6f87cc
ErminaKhan <erminakhan13@gmail.com>
2024-08-18 01:55:02 +0530

    Hello World.c uploaded

---
+++ Basic.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h> //standard library that contains standard input (scanf) and output (printf) functions
+
+int main() 
+//int main= inbuilt function where the actual execution begins
+{
+    printf("Hello World"); //used to print the output on the screen
+
+    return 0; //to indicate that the program has zero errors
+}
--- HelloWorld.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h> //standard library that contains standard inputs (scanf) and outputs (printf)
-
-int main() 
-//int main= inbuilt function where the execution begins
-{
-    printf("Hello World"); //used to print the output on the screen
-
-    return 0; //to indicate that the program has zero errors
-}
